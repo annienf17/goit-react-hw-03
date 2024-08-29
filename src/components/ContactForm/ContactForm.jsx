54e4d14aeacd6a0e2273b8942c8b548c4d04fab4
annienf17 <a.ulanskaxxi@gmail.com>
2024-08-29 14:01:29 +0100

    walidation number

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -11,7 +11,7 @@ const ContactFormSchema = Yup.object().shape({
     .min(3, "Minimalna liczba znaków to 3")
     .max(50, "Maksymalna liczba znaków to 50"),
   number: Yup.string()
-    .matches(/^\d+$/, "Numer może zawierać tylko cyfry")
+    .matches(/^[\d-]+$/, "Numer może zawierać tylko cyfry i znak '-'")
     .required("Wypełnienie pola jest obowiązkowe")
     .min(3, "Minimalna liczba znaków to 3")
     .max(50, "Maksymalna liczba znaków to 50"),
